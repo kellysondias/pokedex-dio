8ca0b5b04cbf90b052ed84d037a8b8115bd43593
Kellyson Dias <kellysondias@outlook.com>
2023-09-06 21:53:43 -0300

    Adding an event listener

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,30 +6,48 @@ let offset = 0;
 const limit = 10;
 
 function loadPokemonItems(offset, limit) {
-  pokeApi.getPokemons(offset, limit).then(
-    (pokemons = []) =>
-      (pokemonList.innerHTML += pokemons
-        .map(
-          (pokemon) => `
-          <li class="pokemon ${pokemon.type}">
-            <span class="number">#${pokemon.number}</span>
-            <span class="name">${pokemon.name}</span>
-            <div class="detail">
-                <ol class="types">
-                  ${pokemon.types
-                    .map((type) => `<li class="type ${type}">${type}</li>`)
-                    .join("")}
-                </ol>
-                <img src="${pokemon.image}"
-                alt="${pokemon.name}">
-            </div>
-          </li>`
-        )
-        .join(""))
-  );
+  pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+    const pokemonItems = pokemons
+      .map((pokemon) => {
+        return `
+        <li class="pokemon ${pokemon.type}">
+         <a href="#"> 
+          <span class="number">#${pokemon.number}</span>
+          <span class="name">${pokemon.name}</span>
+          <div class="detail">
+            <ul class="types">
+             ${pokemon.types
+               .map(
+                 (type) => `
+              <li class="type ${type}">${type}</li>
+             `
+               )
+               .join("")}
+            </ul>
+            <img src="${pokemon.image}" alt="${pokemon.name}" />
+          </div>
+         </a>
+        </li>
+      `;
+      })
+      .join("");
+
+    pokemonList.innerHTML += pokemonItems;
+  });
+}
+
+function addPokemonClickListener() {
+  pokemonList.addEventListener("click", (event) => {
+    if (event.target.classList.contains("pokemon")) {
+      const clickedPokemon = event.target;
+      console.log(clickedPokemon);
+    }
+  });
 }
 
 loadPokemonItems(offset, limit);
+
+window.addEventListener("load", addPokemonClickListener);
 
 loadMoreButton.addEventListener("click", () => {
   offset += limit;
---
+++ assets/js/pokemon/index.js
@@ -0,0 +1,11 @@
+const root = document.getElementById("root");
+
+const loadPokemonDetail = (pokemon) => {
+  return `
+            <div>
+              <h1>pokemon<h1>
+            </div>
+        `;
+};
+
+loadPokemonDetail();
