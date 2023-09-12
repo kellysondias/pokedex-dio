ba3bf8c4847797bfad2351dfe99db9acc64ca8fa
Kellyson Dias <kellysondias@outlook.com>
2023-09-12 16:21:57 -0300

    Update index.js

--- assets/js/pokemon/index.js
+++ assets/js/pokemon/index.js
@@ -4,7 +4,7 @@ const loadPokemonDetail = (pokemon) => {
   console.log("POKEMON:", pokemon.name)
   return `
             <div>
-              <h1>${pokemon.name}<h1>
+              <h1>pokemon.name/<h1>
             </div>
         `;
 };
