03bb64212576ba79d348bc615d152b1aba9c62b9
Kellyson Dias <kellysondias@outlook.com>
2023-09-14 22:04:22 -0300

    Update

--- assets/js/pokemon/index.js
+++ assets/js/pokemon/index.js
@@ -2,12 +2,13 @@ const root = document.getElementById("root");
 
 const goToProfile = (number, name, image, types) => {
   const pokemon = { number, name, image, types};
-  sessionStorage.setItem("pokemon", pokemon);
+  sessionStorage.setItem("pokemon", JSON.stringify(pokemon));
   window.location.href = "pokemon.html";
 };
 
 const pokemonData = JSON.parse(sessionStorage.getItem("pokemon"))
 console.log(pokemonData);
+
 const loadPokemonDetail = (pokemon) => {
   const html = `
             <div>
